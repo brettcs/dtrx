ee2fe7938d269eb8cc44c2d1759c0c3662b7eff6
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2022-04-12 12:02:27 -0400

    Only build docker image for tests that use it
    The windows test doesn't use the main docker image, so make building
    that a function and only run it in the other jobs

--- test.sh
+++ test.sh
@@ -15,12 +15,15 @@ fi
 DOCKER_IMAGE_NAME=${DOCKER_IMAGE_NAME:-$(basename -s .git "$(git remote --verbose | awk 'NR==1 { print tolower($2) }')")}
 
 # build the docker image
-DOCKER_BUILDKIT=1 docker build -t "$DOCKER_IMAGE_NAME" --build-arg "UID=$(id -u)" -f Dockerfile .
+function build_image() {
+    DOCKER_BUILDKIT=1 docker build -t "$DOCKER_IMAGE_NAME" --build-arg "UID=$(id -u)" -f Dockerfile .
+}
 
 # by default, run tox
 RUN_JOB=${RUN_JOB:-tox}
 case $RUN_JOB in
     tox)
+        build_image
         # execute tox in the docker container. don't run in parallel; the test
         # script writes files to an in-tree location, so run serially to avoid
         # clobbering during the tests
@@ -29,10 +32,12 @@ case $RUN_JOB in
         ./tools/test-nonexistent-file-cmd.sh
     ;;
     quick-test)
+        build_image
         # single quick test
         docker run --rm -v "$(pwd)":/workspace -t "$DOCKER_IMAGE_NAME" bash -c "python3 tests/compare.py"
     ;;
     rst2man)
+        build_image
         # build man page from README
         docker run --rm -v "$(pwd)":/workspace -t "$DOCKER_IMAGE_NAME" bash tools/gen-manpage.sh archived/README dtrx.1
     ;;
